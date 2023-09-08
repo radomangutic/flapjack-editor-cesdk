635dbde5967f9a5dbed2cdb3a6b97e3debe8ad1e
RadomanGutic <radomangutic01@gmail.com>
2023-09-08 16:43:05 -0700

    missed typo

--- pages/api/inviteuser.ts
+++ pages/api/inviteuser.ts
@@ -9,7 +9,7 @@ export default async function handler(
 ) {
   const { phone, restaurantName, restaurantId } = JSON.parse(req.body);
   try {
-    const messageBody = `You've been invited to edit menus on Flapjack for ${restaurantName}. Please open the following link on your computer to create an account to join your team: \n${process.env.SITE_DOMAIN}/templates?phone=${phone}&id=${restaurantId}\n`;
+    const messageBody = `You've been invited to edit menus on Flapjack for ${restaurantName}. Please open the following link on your computer to create an account and join your team: \n${process.env.SITE_DOMAIN}/templates?phone=${phone}&id=${restaurantId}\n`;
     const message = await client.messages.create({
       body: messageBody,
       from: process.env.TWILLO_PHONE,
