1272d851e64f6d06838f0f057389cfbd4f5370f4
RadomanGutic <radomangutic01@gmail.com>
2023-09-11 14:12:21 +0500

    sort menus according to restataurant and its location for flapjack user

--- pages/templates.tsx
+++ pages/templates.tsx
@@ -56,7 +56,7 @@ const Templates = ({ thumbnails }: { thumbnails: string[] }) => {
   const templateData = templates?.filter((template) => {
     if (navMenu === "templates") {
       if (
-      // template is template created by user and not global or template is created by user's restaurant
+        // template is template created by user and not global or template is created by user's restaurant
         (template.createdBy === user?.id && !template.isGlobal) ||
         template?.restaurant_id === user?.restaurant_id
       ) {
@@ -87,7 +87,7 @@ const Templates = ({ thumbnails }: { thumbnails: string[] }) => {
       }
       menuMap[currentLocation].push(menuData);
     });
-    const groupedMenus: any[] = locations.map((location) => ({
+    const groupedMenus: any[] = locations?.map((location) => ({
       location,
       menus: menuMap[location] || [],
     }));
@@ -105,6 +105,110 @@ const Templates = ({ thumbnails }: { thumbnails: string[] }) => {
     templateData,
     user?.restaurant?.location || []
   );
+  const ungroupedMenus = templateData?.filter(
+    (template) => !!!template?.restaurant_id
+  );
+  if (user?.role === "flapjack" && resturantsOptions.length) {
+    return (
+      <>
+        <TemplateHeader setNavMenu={setNavMenu} navMenu={navMenu} />
+        <Container size="xl" px="xl" pt={16}>
+          <Text size={32} weight={400} sx={{ marginBottom: "1rem" }}>
+            {router.query.myMenu && navMenu === "templates" ? "" : "My Menus"}
+          </Text>
+          {loading ? (
+            <h1 style={{ textAlign: "center" }}>Loading...</h1>
+          ) : (
+            <>
+              {resturantsOptions.map((item: any, i) => {
+                const menus = item?.location?.length
+                  ? groupMenusByLocation(templateData, item?.location)
+                  : [
+                      {
+                        menus: templateData.filter(
+                          (template) => template?.restaurant_id == item?.value
+                        ),
+                      },
+                    ];
+
+                return (
+                  <div key={i}>
+                    <Text style={{ fontSize: "26px" }} fw={'inherit'} my={"md"}>
+                      {item?.label}
+                    </Text>
+
+                    {menus.map((item: any, i) => (
+                      <div key={i}>
+                        <Text fz={"xl"} fw={'inherit'} my={"md"}>
+                          {item?.location}
+                        </Text>
+                        <SimpleGrid
+                          cols={3}
+                          breakpoints={[
+                            { maxWidth: 1120, cols: 3, spacing: "md" },
+                            { maxWidth: 991, cols: 2, spacing: "sm" },
+                            { maxWidth: 600, cols: 1, spacing: "sm" },
+                          ]}
+                        >
+                          {item?.menus?.length ? (
+                            item?.menus.map((template: any, i: number) => (
+                              <TemplateCard
+                                key={i}
+                                template={template}
+                                thumbnail={thumbnails?.[template.id]}
+                                onRemove={deleteTemplate}
+                                onRename={renameTemplate}
+                                onDuplicate={duplicateTemplate}
+                                //@ts-ignore
+                                onGlobal={globalTemplate}
+                                navMenu={navMenu}
+                                resturantsOptions={resturantsOptions}
+                                setTemplates={setTemplates}
+                              />
+                            ))
+                          ) : (
+                            <Text>No menu found</Text>
+                          )}
+                        </SimpleGrid>
+                      </div>
+                    ))}
+                  </div>
+                );
+              })}
+            </>
+          )}
+          <div>
+            <Text style={{ fontSize: "26px" }} fw={'inherit'} my={"md"}>
+              Menu without restaurant
+            </Text>
+            <SimpleGrid
+              cols={3}
+              breakpoints={[
+                { maxWidth: 1120, cols: 3, spacing: "md" },
+                { maxWidth: 991, cols: 2, spacing: "sm" },
+                { maxWidth: 600, cols: 1, spacing: "sm" },
+              ]}
+            >
+              {ungroupedMenus.map((template: any, i: number) => (
+                <TemplateCard
+                  key={i}
+                  template={template}
+                  thumbnail={thumbnails?.[template.id]}
+                  onRemove={deleteTemplate}
+                  onRename={renameTemplate}
+                  onDuplicate={duplicateTemplate}
+                  //@ts-ignore
+                  onGlobal={globalTemplate}
+                  navMenu={navMenu}
+                  resturantsOptions={resturantsOptions}
+                />
+              ))}
+            </SimpleGrid>
+          </div>
+        </Container>
+      </>
+    );
+  }
 
   return (
     <>
@@ -119,7 +223,9 @@ const Templates = ({ thumbnails }: { thumbnails: string[] }) => {
           <>
             {groupedMenus.map((item: any, i) => (
               <div key={i}>
-                <Text fz={'xl'} fw={'bold'} my={'md'}>{item?.location}</Text>
+                <Text fz={"xl"} fw={"bold"} my={"md"}>
+                  {item?.location}
+                </Text>
                 <SimpleGrid
                   cols={3}
                   breakpoints={[
@@ -180,7 +286,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const supabase = createServerSupabaseClient(context);
   const { data } = await supabase
     .from("templates")
-    .select("id, createdBy, name, description, tags, isGlobal, menuSize, location")
+    .select(
+      "id, createdBy, name, description, tags, isGlobal, menuSize, location"
+    )
     .order("templateOrder", { ascending: true });
 
   return {
