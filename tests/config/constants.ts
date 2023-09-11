1afe9af7a8afbaf57d10de96f073e7a300d21906
RadomanGutic <radomangutic01@gmail.com>
2023-09-11 12:43:09 +0500

    config playwright and write test

--- tests/config/constants.ts
+++
@@ -1,5 +0,0 @@
-import path from 'path'
-
-export const FLAPJACK_FILE = path.resolve(__dirname, "../.auth/flapjack.json");
-export const PAID_USER_FILE = path.resolve(__dirname, "../.auth/paid_user.json");
-export const USER_FILE = path.resolve(__dirname, "../.auth/user.json");
---
+++ tests/tests.spec.ts
@@ -0,0 +1,69 @@
+import { test, expect } from "@playwright/test";
+
+test("Login with phone", async ({ page }) => {
+  await page.goto("http://localhost:3000/templates");
+  await page.getByPlaceholder("Enter your phone number").click();
+  await page
+    .getByPlaceholder("Enter your phone number")
+    .fill(`1208568${getRandomChar("0123456789")}`);
+  await page.getByRole("button", { name: "Log in with Phone" }).click();
+  await expect(page.getByRole("button", { name: "Verify Otp" })).toBeVisible();
+});
+
+test("Login with email", async ({ page }) => {
+  await page.goto("http://localhost:3000/templates");
+  await page.getByRole("button", { name: "Log in with Email" }).click();
+  await page.getByPlaceholder("Enter your email address").click();
+  await page
+    .getByPlaceholder("Enter your email address")
+    .fill(
+      `test${getRandomChar("abcdefghijklmnopqrstuvwxyz0123456789")}@email.com`
+    );
+  await page.getByRole("button", { name: "Log in with Email" }).click();
+  await expect(page.getByText("Please check your email")).toBeVisible();
+});
+
+test("Save menu", async ({ page }) => {
+  await page.goto("http://localhost:3000/menu/preview/103");
+  await page.getByLabel("Export Images").click();
+  await expect(
+    page.getByRole("button", { name: "Log in with Phone" })
+  ).toBeVisible();
+});
+
+test("Create menu", async ({ page }) => {
+  await page.goto("http://localhost:3000/template");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByPlaceholder("Enter your phone number")).toBeVisible();
+});
+
+function getRandomChar(inputString: string): string | null {
+  const digits = inputString.match(/\d/g);
+
+  // Check if there are enough digits to form a four-digit string
+  if (digits && digits.length >= 4) {
+    const randomIndices: any = [];
+    const resultDigits = [];
+
+    // Generate four unique random indices
+    while (randomIndices.length < 4) {
+      const randomIndex = Math.floor(Math.random() * digits.length);
+
+      // Ensure the generated index is unique
+      if (!randomIndices.includes(randomIndex)) {
+        randomIndices.push(randomIndex);
+      }
+    }
+
+    // Retrieve the characters at the random indices
+    for (const index of randomIndices) {
+      resultDigits.push(digits[index]);
+    }
+
+    // Combine the digits to form a four-digit string
+    return resultDigits.join("");
+  }
+
+  // Return null if there are not enough digits
+  return null;
+}
