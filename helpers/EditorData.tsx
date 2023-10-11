bd344305bd0c5165b65924e9c3d48d46fa15d557
RadomanGutic <radomangutic01@gmail.com>
2023-10-11 20:34:25 +0500

    resolve error

--- helpers/EditorData.tsx
+++ helpers/EditorData.tsx
@@ -3,7 +3,8 @@ import { GetServerSidePropsContext } from "next";
 import { convertToSectionList } from "./convertToSectionList";
 
 export async function getEditorData(context: GetServerSidePropsContext) {
-  const supabase = createServerSupabaseClient(context);
+  try {
+    const supabase = createServerSupabaseClient(context);
   const { data } = await supabase
     .from("templates")
     .select(
@@ -12,33 +13,33 @@ export async function getEditorData(context: GetServerSidePropsContext) {
     .eq("id", context?.params?.id);
   let elementList: any;
 
-  const elements = await supabase
-    .from("ElementLibrary")
-    .select("*")
-    .order("created_at", { ascending: false });
-  elementList = elements?.data?.map((item, i) => {
-    const imagePath = `${process.env.NEXT_PUBLIC_SUPABASE_URL
-      }/storage/v1/object/public/elementsThumbnail/${item.thumbnail
-      }?${i}${Date.now()}`;
+  // const elements = await supabase
+  //   .from("ElementLibrary")
+  //   .select("*")
+  //   .order("created_at", { ascending: false });
+  // elementList = elements?.data?.map((item, i) => {
+  //   const imagePath = `${process.env.NEXT_PUBLIC_SUPABASE_URL
+  //     }/storage/v1/object/public/elementsThumbnail/${item.thumbnail
+  //     }?${i}${Date.now()}`;
 
-    return {
-      id: item?.id?.toString(),
-      createdBy: item?.createdBy || null,
-      restaurant_id: item?.restaurant_id,
-      meta: {
-        uri: "https://img.ly/static/ubq_samples/imgly_logo.jpg",
-        blockType: "//ly.img.ubq/text",
-        thumbUri: imagePath,
-        width: 100,
-        height: 10,
-        value: item?.element,
-        name: item?.restaurant_id,
-      },
-      context: {
-        sourceId: "Custom component",
-      },
-    };
-  });
+  //   return {
+  //     id: item?.id?.toString(),
+  //     createdBy: item?.createdBy || null,
+  //     restaurant_id: item?.restaurant_id,
+  //     meta: {
+  //       uri: "https://img.ly/static/ubq_samples/imgly_logo.jpg",
+  //       blockType: "//ly.img.ubq/text",
+  //       thumbUri: imagePath,
+  //       width: 100,
+  //       height: 10,
+  //       value: item?.element,
+  //       name: item?.restaurant_id,
+  //     },
+  //     context: {
+  //       sourceId: "Custom component",
+  //     },
+  //   };
+  // });
 
   const sortResData = () => {
     const sectionedList = Object.values(convertToSectionList(elementList));
@@ -77,34 +78,37 @@ export async function getEditorData(context: GetServerSidePropsContext) {
     });
     return Promise.all(responseList);
   };
-  const sortedData = await sortResData();
-  const globalTemplates = await sortAssetsImages();
-  const response = await globalTemplates?.map((item) => {
-    const exist = sortedData?.find(
-      (i) => i?.restaurant_id === item?.restaurant_id
-    );
-    if (exist) {
-      return {
-        ...item,
-        resturantDetail: item?.resturantDetail
-          ? {
-            ...item?.resturantDetail,
-            name: `${item?.resturantDetail?.name}.`,
-          }
-          : {
-            name: `Others.`,
-          },
-      };
-    }
-    return item;
-  });
+  // const sortedData = await sortResData();
+  // const globalTemplates = await sortAssetsImages();
+  // const response = await globalTemplates?.map((item) => {
+  //   const exist = sortedData?.find(
+  //     (i) => i?.restaurant_id === item?.restaurant_id
+  //   );
+  //   if (exist) {
+  //     return {
+  //       ...item,
+  //       resturantDetail: item?.resturantDetail
+  //         ? {
+  //           ...item?.resturantDetail,
+  //           name: `${item?.resturantDetail?.name}.`,
+  //         }
+  //         : {
+  //           name: `Others.`,
+  //         },
+  //     };
+  //   }
+  //   return item;
+  // });
 
   return {
     props: {
       data: data && context?.params?.id ? data[0] : null,
       elementsList: [], // hotfix: this was causing the payload to exceed the limit. When we release this feature, we'll need to make sure that this payload doesn't break the page. We can probably fetch this data client side or when the panel is opened.
-      sectionedList: sortedData,
-      globalTemplates: response,
+      sectionedList: [],
+      globalTemplates: [],
     },
   };
+  } catch (error) {
+    console.error(error);
+  }
 }
