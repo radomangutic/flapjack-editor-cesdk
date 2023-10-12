09ac0ad94c1e24182b604eb2bb55daffa89bc484
RadomanGutic <radomangutic01@gmail.com>
2023-10-12 16:16:27 +0500

    add error handling

--- helpers/EditorData.tsx
+++ helpers/EditorData.tsx
@@ -1,27 +1,29 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
 import { getLogedInUser } from "../tests/helpers/database.helper";
-import {
-  getRestaurantsByLogedInUser,
-} from "../tests/helpers/menu.helper";
+import { getRestaurantsByLogedInUser } from "../tests/helpers/menu.helper";
 
 export async function getEditorData(context: GetServerSidePropsContext) {
-  const supabase = createServerSupabaseClient(context);
-  const logedInUser = await getLogedInUser(supabase);
-  const { data } = await supabase
-    .from("templates")
-    .select(
-      "id, createdBy, name, description, content, tags, isGlobal, menuSize, restaurant_id, location"
-    )
-    .eq("id", context?.params?.id);
+  try {
+    const supabase = createServerSupabaseClient(context);
+    const logedInUser = await getLogedInUser(supabase);
+    const { data } = await supabase
+      .from("templates")
+      .select(
+        "id, createdBy, name, description, content, tags, isGlobal, menuSize, restaurant_id, location"
+      )
+      .eq("id", context?.params?.id);
 
-  const restaurantList = await getRestaurantsByLogedInUser(logedInUser);
-  
-  return {
-    props: {
-      data: data && context?.params?.id ? data[0] : null,
-      restaurantList: restaurantList,
-      user: logedInUser,
-    },
-  };
+    const restaurantList = await getRestaurantsByLogedInUser(logedInUser);
+
+    return {
+      props: {
+        data: data && context?.params?.id ? data[0] : null,
+        restaurantList: restaurantList,
+        user: logedInUser,
+      },
+    };
+  } catch (error) {
+    throw error;
+  }
 }
