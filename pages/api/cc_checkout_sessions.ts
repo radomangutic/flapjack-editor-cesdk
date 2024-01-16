07bb5ea1a9bbb0594c82869e2329f255330a369d
RadomanGutic <radomangutic01@gmail.com>
2024-01-16 12:43:32 -0800

    feature add message describing trial cancellation

--- pages/api/cc_checkout_sessions.ts
+++ pages/api/cc_checkout_sessions.ts
@@ -19,7 +19,7 @@ export default async function handler(
           custom_text: {
             submit: {
               message: `If you decide to cancel your trial, you will not be charged. If you choose to keep your plan after the trial, the 2-week period will be considered part of your first month's service.`,
-            },
+            }
           },
           payment_method_options: {
             us_bank_account: {
@@ -30,10 +30,7 @@ export default async function handler(
             },
           },
           consent_collection: {
-            terms_of_service: "none",
-            payment_method_reuse_agreement: {
-              position: 'hidden'
-            }
+            terms_of_service: "required"
           },
 
           payment_method_types: ["card"],
